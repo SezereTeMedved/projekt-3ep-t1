cf670944310638be5b69cbca55e3ec98bffe1104
Vojtěch Martin <vojtech@spstrutnov.cz>
2020-02-06 17:56:08 +0100

    Rozdělaná navigace

--- php/Index/index.php
+++ php/Index/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/style.css">
     <title>Škola pro mě</title>
 </head>
 <body>
@@ -11,13 +12,44 @@
         <h1>Škola pro mě</h1>
         
     </header>
+    <nav>
+
+        <div class="navZS">
+            <button>Základní školy</button>
+            <div class="navZS_list">
+                <a href="">Klasické</a>
+                <a href="">Speciální</a>
+            </div>
+        </div>
+
+        <div class="navSS">
+            <button>Střední školy</button>
+            <div class="navSS_list">
+                <a href="">Gymnazia</a>
+                <a href="">Odborné</a>
+                <a href="">Učňovské</a>
+            </div>
+        </div>
+
+        <div class="navVS">
+            <button>Vysoké školy</button>
+            <div class="navVS_list">
+                <a href="">Univerzitní</a>
+                <a href="">Neuniverzitní</a>
+            </div>
+        </div>
+      
+        <a href="">Přidat školu</a>
+    </nav>
 
     <main>
         <article>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe expedita hic consequuntur recusandae architecto modi aspernatur sed perferendis amet beatae, odit maxime! Officia laboriosam minima, nihil vel sit iure beatae.
         </article>
     </main>
 
     <footer>
+        <p>&copy; Provazník, Trojánek, Vojtěch</p>
     </footer>
 </body>
 </html>
